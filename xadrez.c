835526b2e0b0d29dce928920534bcec3a1cabdd6
matheus0-9 <matheus68501@gmail.com>
2025-10-24 01:05:32 +0000

    fiz o esqueleto do jogo de xadrez

--- xadrez.c
+++ xadrez.c
@@ -1,7 +1,29 @@
 #include<stdio.h>
 
     int main(){
+
         printf("Desafio Xadrez!\n");
+        //Movimento da torre para dereita!
+        for( int i = 0; i < 5; i++){
+            printf("Direita\n");// Movimentação da torre para a direita!
+        }
+        //MOVIMENTAÇÃO DA RAINHA PARA ESQUERDA!
+        int i = 1;
+        while (i <= 8) {
+            printf("esquerda\n");
+            i++;
+        }
+        //MOVIMENTAÇÃO DO BISPO 5 CASAS NA DIAGONAL DIREUTA!
+        int i = 1;  
+        printf("\nMovimentação do bispo!\n");
+        do {
+            printf("Diagonal direita\n");
+            i++;
+        } while (i <= 5);
+        
+        
+    
+
 
 
 
